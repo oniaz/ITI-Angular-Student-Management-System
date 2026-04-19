5027c5f05eb4e2d271602a69c6208fb5e721e4c2
oniaz <32391049+oniaz@users.noreply.github.com>
2026-04-19 05:11:13 +0200

    feat: implement add student form

--- src/app/pages/add-student/add-student.component.ts
+++ src/app/pages/add-student/add-student.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { StudentsService } from '../../services/students.service';
 import { Student } from '../../models/student';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-student',
@@ -8,7 +9,8 @@ import { Student } from '../../models/student';
   styleUrls: ['./add-student.component.css']
 })
 export class AddStudentComponent {
-  constructor(private _studentsService: StudentsService) { }
+  // constructor(private _studentsService: StudentsService) { }
+  constructor(private _studentsService: StudentsService, private router: Router) { }
 
   studentsData$ = this._studentsService.currentData$;
 
@@ -30,9 +32,40 @@ export class AddStudentComponent {
     isActive: true
   };
 
-  addStudent() {
+  addStudentGahez() {
     const updated = [...this._studentsService['studentsData'].value, this.newStudent];
     this._studentsService.addStudent(updated);
+  }
+
+  addStudent(
+    firstName: string, lastName: string, age: string, gender: string,
+    email: string, phone: string, address: string, city: string,
+    country: string, department: string, level: string, GPA: string,
+    enrollmentDate: string, isActive: boolean
+  ) {
+    // Construct the object
+    const newStudent = {
+      id: Math.floor(Math.random() * 10000), // Unique ID generator
+      firstName,
+      lastName,
+      age: Number(age),
+      gender,
+      email,
+      phone,
+      address,
+      city,
+      country,
+      department,
+      level: Number(level),
+      GPA: Number(GPA),
+      enrollmentDate,
+      isActive
+    };
+
+    const updated = [...this._studentsService['studentsData'].value, newStudent];
+    this._studentsService.addStudent(updated);
+
+    this.router.navigate(['/students']);
   }
 
 }
